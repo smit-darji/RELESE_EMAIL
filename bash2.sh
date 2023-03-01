a055ca72df52c332290e8fac6e66f439cbc06307
YMakwanaIH <yogesh.makwana@invitationhomes.com>
2023-03-01 19:10:35 +0530

    sadiuyasduytgaduya

--- bash2.sh
+++ bash2.sh
@@ -3,50 +3,52 @@ releases='[
     "name": "v1.0.6",
     "tag": "v1.0.6",
     "url": "https://github.com/smit-darji/RELESE_EMAIL/releases/tag/v1.0.6",
-    "body": ""
+    "body": "sdsadsd"
   },
   {
     "name": "v1.0.4",
     "tag": "v1.0.4",
     "url": "https://github.com/smit-darji/RELESE_EMAIL/releases/tag/v1.0.4",
-    "body": ""
+    "body": "sadadasdsad"
   },
   {
     "name": "v1.0.3",
     "tag": "v1.0.3",
     "url": "https://github.com/smit-darji/RELESE_EMAIL/releases/tag/v1.0.3",
-    "body": ""
+    "body": "asdsadsadad"
   },
   {
     "name": "v1.0.2",
     "tag": "v1.0.2",
     "url": "https://github.com/smit-darji/RELESE_EMAIL/releases/tag/v1.0.2",
-    "body": ""
+    "body": "sadadadad"
   },
   {
     "name": "v1.0.1",
     "tag": "v1.0.1",
     "url": "https://github.com/smit-darji/RELESE_EMAIL/releases/tag/v1.0.1",
-    "body": ""
+    "body": "asdadadada"
   },
   {
-    "name": "relese v1.0.0",
+    "name": "v1.0.0",
     "tag": "v1.0.0",
     "url": "https://github.com/smit-darji/RELESE_EMAIL/releases/tag/v1.0.0",
-    "body": ""
+    "body": "asdadad"
   }
 ]'
 
-# Loop over each release in the JSON array
+all_messages=""
+
 for release in $(echo "$releases" | jq -c '.[]'); do
-    # Extract name, tag, and body from release object
     name=$(echo "$release" | jq -r '.name')
     tag=$(echo "$release" | jq -r '.tag')
-    body=$(echo "$release" | jq -r '.body')
+    url=$(echo "$release" | jq -r '.url')
+    body=$(echo "$release" | jq -r '.body')    
+    message="Release $name ($tag) is now available. Download it from ($url)}.\n Description is :$body\n\n"
     
-    # Format release message
-    message="Release $name ($tag):\n$body\n\n"
-    
-    # Print release message
-    echo -e "$message"
+    all_messages="$all_messages$message" 
 done
+
+echo -e "$all_messages"
+
+
