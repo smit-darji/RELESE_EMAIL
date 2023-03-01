75f5667108edb551397e175474cf0a219c3c2f51
YMakwanaIH <yogesh.makwana@invitationhomes.com>
2023-03-01 18:45:13 +0530

    sadiuyasduytgaduya

--- bash2.sh
+++ bash2.sh
@@ -1,62 +1,8 @@
 releases='[
   {
-    "name": "v1.0.16",
-    "tag": "v1.0.16",
-    "url": "https://github.com/smit-darji/RELESE_EMAIL/releases/tag/v1.0.16",
-    "body": "v1.0.16"
-  },
-  {
-    "name": "v1.0.15",
-    "tag": "v1.0.15",
-    "url": "https://github.com/smit-darji/RELESE_EMAIL/releases/tag/v1.0.15",
-    "body": "v1.0.15"
-  },
-  {
-    "name": "v1.0.14",
-    "tag": "v1.0.14",
-    "url": "https://github.com/smit-darji/RELESE_EMAIL/releases/tag/v1.0.14",
-    "body": ""
-  },
-  {
-    "name": "v1.0.13",
-    "tag": "v1.0.13",
-    "url": "https://github.com/smit-darji/RELESE_EMAIL/releases/tag/v1.0.13",
-    "body": "v1.0.13"
-  },
-  {
-    "name": "v1.0.12",
-    "tag": "v1.0.12",
-    "url": "https://github.com/smit-darji/RELESE_EMAIL/releases/tag/v1.0.12",
-    "body": "v1.0.12"
-  },
-  {
-    "name": "v1.0.11",
-    "tag": "v1.0.11",
-    "url": "https://github.com/smit-darji/RELESE_EMAIL/releases/tag/v1.0.11",
-    "body": "v1.0.11"
-  },
-  {
-    "name": "v1.0.8",
-    "tag": "v1.0.8",
-    "url": "https://github.com/smit-darji/RELESE_EMAIL/releases/tag/v1.0.8",
-    "body": "v1.0.8"
-  },
-  {
-    "name": "v1.0.7",
-    "tag": "v1.0.7",
-    "url": "https://github.com/smit-darji/RELESE_EMAIL/releases/tag/v1.0.7",
-    "body": "v1.0.7"
-  },
-  {
     "name": "v1.0.6",
     "tag": "v1.0.6",
     "url": "https://github.com/smit-darji/RELESE_EMAIL/releases/tag/v1.0.6",
-    "body": ""
-  },
-  {
-    "name": "v1.0.5",
-    "tag": "v1.0.5",
-    "url": "https://github.com/smit-darji/RELESE_EMAIL/releases/tag/v1.0.5",
     "body": ""
   },
   {
@@ -84,7 +30,7 @@ releases='[
     "body": ""
   },
   {
-    "name": "v1.0.0",
+    "name": "relese v1.0.0",
     "tag": "v1.0.0",
     "url": "https://github.com/smit-darji/RELESE_EMAIL/releases/tag/v1.0.0",
     "body": ""
