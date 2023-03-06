05046a90863ee41d8cf1993e993790347d820be2
YMakwanaIH <yogesh.makwana@invitationhomes.com>
2023-03-06 16:54:41 +0530

    dsf

--- a.py
+++ a.py
@@ -1,18 +1,1 @@
-import os
-import requests
-from datetime import datetime, timedelta
-
-def get_release_data():
-    api_url = "https://api.github.com/repos/{owner}/{repo}/releases"
-    owner = os.environ["GITHUB_REPOSITORY_OWNER"]
-    repo = os.environ["GITHUB_REPOSITORY_NAME"]
-    headers = {
-        "Accept": "application/vnd.github.v3+json",
-        "Authorization": f"Bearer {os.environ['GITHUB_TOKEN']}"
-    }
-    today = datetime.utcnow().date()
-    week_ago = today - timedelta(days=7)
-    params = {
-        "per_page": 100,
-        "page": 1,
-        "since": week_ago.isoformat()
+print("hello")
