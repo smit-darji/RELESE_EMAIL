f0013b23a2d4e49db41db84378608b8998dfd53a
YMakwanaIH <yogesh.makwana@invitationhomes.com>
2023-03-06 14:53:50 +0530

    sdsdf

--- send_release_email.py
+++ send_release_email.py
@@ -1,43 +1,19 @@
-import os
-import requests
-from datetime import datetime, timedelta
-from jinja2 import Environment, FileSystemLoader
+from github import Github
+from jinja2 import Template
 
-# Set up the Jinja2 environment
-env = Environment(loader=FileSystemLoader('.'))
+# Create a Github instance
+g = Github()
 
-# Define the GitHub API endpoint
-api_url = 'https://api.github.com'
+# Get a repository by its owner and name
+repo = g.get_repo("smit-darji/RELESE_EMAIL")
 
-# Get the repository name and owner from the environment variables
-# repo_name = os.environ.get('GITHUB_REPOSITORY')
-# repo_owner = repo_name.split('/')[0]
+# Get the latest release
+latest_release = repo.get_latest_release()
 
-# Set up the authentication headers
-auth_header = {
-    'Authorization': f'token {os.environ.get("GITHUB_TOKEN")}',
-    'Accept': 'application/vnd.github.v3+json'
-}
+# Format the release details as a string
+release_details = f"Release name: {latest_release.title}\nRelease tag: {latest_release.tag_name}\nRelease body: {latest_release.body}"
+template = Template("{{ release_details }}")
 
-# Define the start and end dates for the release query
-end_date = datetime.now()
-start_date = end_date - timedelta(days=7)
-
-# Query the GitHub API for releases in the last week
-response = requests.get(
-    f'{api_url}/repos/smit-darji/RELESE_EMAIL/releases',
-    headers=auth_header
-)
-
-# Filter the releases by date
-releases = [
-    release for release in response.json()
-    if start_date <= datetime.fromisoformat(release['published_at'][:-1]) <= end_date
-]
-
-# Render the email template using Jinja2
-template = env.get_template('release_email_template.j2')
-email_body = template.render(repo_owner=smit-darji, repo_name=RELESE_EMAIL, releases=releases)
-
-# Output the email body
+# Render the template with the release details
+email_body = template.render(release_details=release_details)
 print(email_body)
